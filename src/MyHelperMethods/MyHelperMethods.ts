209922297470b3259531d0227e6240bdcc8d888f
Steven Chorkawy <schorkawy@clarington.net>
2024-01-19 09:46:11 -0500

    Updating spfi provider.

--- src/MyHelperMethods/MyHelperMethods.ts
+++ src/MyHelperMethods/MyHelperMethods.ts
@@ -1,11 +1,11 @@
 import { WebPartContext } from "@microsoft/sp-webpart-base";
-import { SPFI, SPFx, spfi } from "@pnp/sp";
+import { ISPFXContext, SPFI, SPFx, spfi } from "@pnp/sp";
 
 let _sp: SPFI;
 
 export const getSP = (context?: WebPartContext): SPFI => {
-    if (_sp === null && context !== null) {
-        _sp = spfi().using(SPFx(context as WebPartContext));
+    if (context) {
+        _sp = spfi().using(SPFx(context as ISPFXContext));
     }
     return _sp;
 };
